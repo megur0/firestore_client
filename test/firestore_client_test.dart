13c18a0b5e8473491c6d104252b507bdf91a2402
megur0 <119830503+megur0@users.noreply.github.com>
2024-05-31 08:12:47 +0900

    fix pubspec

--- test/firestore_client_test.dart
+++ test/firestore_client_test.dart
@@ -1,6 +1,6 @@
 import 'package:clock/clock.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_stub/firebase_stub.dart';
+import 'package:cloud_firestore_stub/firebase_stub.dart';
 import 'package:firestore_client/firestore_client.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
